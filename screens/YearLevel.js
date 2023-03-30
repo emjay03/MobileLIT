97a9214ffbba5978489194243113085945638796
MJ <mjcariso3@gmail.com>
2023-03-30 21:43:25 +0800

    enchance ui for home

--- screens/YearLevel.js
+++ screens/YearLevel.js
@@ -1,13 +1,11 @@
 import {View, Text, TouchableHighlight, StyleSheet} from 'react-native';
-import React, { useState, useEffect } from 'react';
-
-
+import React, {useState, useEffect} from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const YearLevel = ({navigation}) => {
-
   return (
     <View>
-      <View className="flex px-10 justify-center h-full bg-white ">
+      <View className="flex px-10 justify-center h-full bg-[#fafafa] ">
         <View className="pb-4">
           <Text style={styles.KarlaItalic} className="text-xl text-[#272727]">
             "Programming isn't about what you know; it's about what you can{' '}
@@ -18,93 +16,123 @@ const YearLevel = ({navigation}) => {
           </Text>
         </View>
         <Text style={styles.SemiBold} className="text-xl  text-[#272727] py-4">
-        What year level are you in?
+          What year level are you in?
         </Text>
-        <View className="">
-         
-        <View className="flex flex-row  w-[100%] rounded-lg border-2 border-[#50D890]">
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
-          <TouchableHighlight
-            className=" w-[74%]  py-4 flex justify-center items-center"
-            onPress={() => navigation.navigate('First Year Module')}
-            underlayColor="#e2e2e2">
-          
+
+        <View className="flex flex-row  w-[100%] bg-white border-2 rounded-xl border-[#f5f5f5]">
+          <View className=" w-[80%]  py-4 flex justify-center items-center">
             <Text
               style={styles.SemiBold}
-              className="text-black text-[18px] py-1">
+              className=" text-[#272727] text-[18px] py-1">
               First Year
             </Text>
-            
-          </TouchableHighlight>
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
           </View>
-          <View className="py-2"></View>
-          <View className="flex flex-row  w-[100%] rounded-lg border-2 border-[#50D890]">
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
-          <TouchableHighlight
-            className=" w-[74%]  py-4 flex justify-center items-center"
-            onPress={() => navigation.navigate('Second Year Module')}
-            underlayColor="#e2e2e2">
-          
+          <View className="w-[20%]  flex items-center justify-center   ">
+            <TouchableHighlight
+              onPress={() => navigation.navigate('First Year Module')}
+              underlayColor={{color: '#e2e2e2', borderRadius: 10}}>
+              <View className="bg-[#50D890] p-3 rounded-3xl">
+                <MaterialIcons
+                  name="arrow-forward-ios"
+                  size={18}
+                  color="#EFFFFB"
+                />
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <View className="py-2"></View>
+
+        <View className="flex flex-row  w-[100%] bg-white border-2 rounded-xl border-[#f5f5f5]">
+          <View className=" w-[80%]  py-4 flex justify-center items-center">
             <Text
               style={styles.SemiBold}
-              className="text-black text-[18px]">
+              className=" text-[#272727] text-[18px] py-1">
               Second Year
             </Text>
-            
-          </TouchableHighlight>
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
           </View>
-          <View className="py-2"></View>
-          <View className="flex flex-row  w-[100%] rounded-lg border-2 border-[#50D890]">
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
-          <TouchableHighlight
-            className=" w-[74%]  py-4 flex justify-center items-center"
-            onPress={() => navigation.navigate('Third Year Module')}
-            underlayColor="#e2e2e2">
-          
+          <View className="w-[20%]  flex items-center justify-center   ">
+            <TouchableHighlight
+              onPress={() => navigation.navigate('Second Year Module')}
+              underlayColor={{color: '#e2e2e2', borderRadius: 10}}>
+              <View className="bg-[#50D890] p-3 rounded-3xl">
+                <MaterialIcons
+                  name="arrow-forward-ios"
+                  size={18}
+                  color="#EFFFFB"
+                />
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+
+        <View className="py-2"></View>
+        <View className="flex flex-row  w-[100%] bg-white border-2 rounded-xl border-[#f5f5f5]">
+          <View className=" w-[80%]  py-4 flex justify-center items-center">
             <Text
               style={styles.SemiBold}
-              className="text-black text-[18px]">
+              className=" text-[#272727] text-[18px] py-1">
               Third Year
             </Text>
-            
-          </TouchableHighlight>
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
           </View>
-          <View className="py-2"></View>
-          <View className="flex flex-row  w-[100%] rounded-lg border-2 border-[#50D890]">
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
-          <TouchableHighlight
-            className=" w-[74%]  py-4 flex justify-center items-center"
-            onPress={() => navigation.navigate('Fourth Year Module')}
-            underlayColor="#e2e2e2">
-          
+          <View className="w-[20%]  flex items-center justify-center   ">
+            <TouchableHighlight
+              onPress={() => navigation.navigate('Third Year Module')}
+              underlayColor={{color: '#e2e2e2', borderRadius: 10}}>
+              <View className="bg-[#50D890] p-3 rounded-3xl">
+                <MaterialIcons
+                  name="arrow-forward-ios"
+                  size={18}
+                  color="#EFFFFB"
+                />
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <View className="py-2"></View>
+        <View className="flex flex-row  w-[100%] bg-white border-2 rounded-xl border-[#f5f5f5]">
+          <View className=" w-[80%]  py-4 flex justify-center items-center">
             <Text
               style={styles.SemiBold}
-              className="text-black text-[18px]">
+              className=" text-[#272727] text-[18px] py-1">
               Fourth Year
             </Text>
-            
-          </TouchableHighlight>
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
           </View>
-          <View className="py-2"></View>
-          <View className="flex flex-row  w-[100%] rounded-lg border-2 border-[#50D890]">
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
-          <TouchableHighlight
-            className=" w-[74%]  py-4 flex justify-center items-center"
-            onPress={() => navigation.navigate('Advance Year Module')}
-            underlayColor="#e2e2e2">
-          
+          <View className="w-[20%]  flex items-center justify-center   ">
+            <TouchableHighlight
+              onPress={() => navigation.navigate('Fourth Year Module')}
+              underlayColor={{color: '#e2e2e2', borderRadius: 10}}>
+              <View className="bg-[#50D890] p-3 rounded-3xl">
+                <MaterialIcons
+                  name="arrow-forward-ios"
+                  size={18}
+                  color="#EFFFFB"
+                />
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <View className="py-2"></View>
+        <View className="flex flex-row  w-[100%] bg-white border-2 rounded-xl border-[#f5f5f5]">
+          <View className=" w-[80%]  py-4 flex justify-center items-center">
             <Text
               style={styles.SemiBold}
-              className="text-black text-[18px]">
-              Advance Level
+              className=" text-[#272727] text-[18px] py-1">
+              Advance Year
             </Text>
-            
-          </TouchableHighlight>
-          <View className="w-[13%]  bg-[#50D890] "><Text></Text></View>
+          </View>
+          <View className="w-[20%]  flex items-center justify-center   ">
+            <TouchableHighlight
+              onPress={() => navigation.navigate('Advance Year Module')}
+              underlayColor={{color: '#e2e2e2', borderRadius: 10}}>
+              <View className="bg-[#50D890] p-3 rounded-3xl">
+                <MaterialIcons
+                  name="arrow-forward-ios"
+                  size={18}
+                  color="#EFFFFB"
+                />
+              </View>
+            </TouchableHighlight>
           </View>
         </View>
       </View>
@@ -117,6 +145,9 @@ const styles = StyleSheet.create({
   },
   Bold: {
     fontFamily: 'Poppins-Bold',
+  },
+  Regular: {
+    fontFamily: 'Poppins-Regular',
   },
   KarlaItalic: {
     fontFamily: 'Karla-Italic',
